eb40b91016a382216d1c4d32e4e7f7e74d6d5fdb
kkr0913 <40028293+kkr0913@users.noreply.github.com>
2019-12-04 17:19:35 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var t = '';
+
+function setup() {
+	textAlign(CENTER, CENTER);
+  createCanvas(windowWidth, windowHeight);
+  updateJson('https://api.myjson.com/bins/rbfo', {'testing at': 'openprocessing', 'one': 'two'});
+}
+
+function draw() {
+  background(220);
+	text(t, 0, 0, width, height);
+}
+
+function updateJson(url, json) {
+    var obj = json;
+    var data = JSON.stringify(obj);
+    $.ajax({
+        url:url,
+        type:"PUT",
+        data: data,
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(data, textStatus, jqXHR){
+            t = 'success'
+        }
+    });
+}
